35938fc6c2cdc13f7b40af62cf86e4ddc38b35ab
Asti22 <uun3461@gmail.com>
2025-12-18 15:56:13 +0700

    menampilkan halaman dengan TopAppBar yang “nempel” (tidak ikut menghilang) saat di-scroll.

--- app/src/main/java/com/example/localapi/view/uicontroller/HalamanHome.kt
+++ app/src/main/java/com/example/localapi/view/uicontroller/HalamanHome.kt
@@ -1,15 +1,34 @@
 package com.example.localapi.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.localapi.view.route.DestinasiHome
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigateToItemEntry:()-> Unit,
     navigateToItemUpdate :(Int)-> Unit
-
-
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior ()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior=scrollBehavior
+            )
+        },
+    )
 
 }
