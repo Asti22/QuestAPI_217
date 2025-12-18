b0d6fc99be4474c78cb1dbd49bac4ef3e4ced9ad
Asti22 <uun3461@gmail.com>
2025-12-18 18:08:14 +0700

    Dependency Injection manual untuk ViewModel

---
+++ app/src/main/java/com/example/localapi/viewmodel/provider/penyediaviewmodel.kt
@@ -0,0 +1,34 @@
+package com.example.localapi.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.localapi.AplikasiDataSiswa
+import com.example.localapi.repositori.DefaultContainerApp
+import com.example.localapi.viewmodel.EntryViewModel
+import com.example.localapi.viewmodel.HomeViewModel
+
+// Ambil Application (AplikasiDataSiswa) dari CreationExtras
+fun CreationExtras.aplikasiDataSiswa(): DefaultContainerApp.AplikasiDataSiswa =
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as DefaultContainerApp.AplikasiDataSiswa
+
+// Factory untuk menyediakan ViewModel
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+    }
+}
