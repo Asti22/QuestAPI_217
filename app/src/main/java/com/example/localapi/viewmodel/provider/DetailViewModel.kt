50efa621c7e6c683f178b02a3e9b470f9e8b1704
Asti22 <uun3461@gmail.com>
2025-12-25 15:39:04 +0700

    tambah logic pengambilan detail siswa pada DetailViewModel

--- app/src/main/java/com/example/localapi/viewmodel/provider/DetailViewModel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/provider/DetailViewModel.kt
@@ -1,7 +1,13 @@
 @file:OptIn(kotlinx.serialization.InternalSerializationApi::class)
 package com.example.localapi.viewmodel.provider
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.localapi.modeldata.DataSiswa
+import com.example.localapi.repositori.RepositoryDataSiswa
 
 
 sealed interface StatusUIDetail {
@@ -9,4 +15,12 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
 
+}
