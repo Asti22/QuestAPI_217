9a2730cace792276e8a90b88eae8444a1a6e324c
Asti22 <uun3461@gmail.com>
2025-12-25 15:53:36 +0700

    tambahkan DetailSiswaScreen dengan ViewModel dan navigasi

---
+++ app/src/main/java/com/example/localapi/view/HalamanDetail.kt
@@ -0,0 +1,15 @@
+package com.example.localapi.view
+
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
